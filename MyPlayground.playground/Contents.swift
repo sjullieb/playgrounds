51ebe184693b294dd22ba92b9803aeae54aa0e8d
Yulia Shidlovskaya <yulia@>
2019-06-06 19:45:41 -0700

    do first touch

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,71 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+enum Rank: Int{
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+
+let ace = Rank.ace
+let aceRawValue = ace.rawValue
+print(ace)
+print(aceRawValue)
+
+if let convertedRank = Rank(rawValue: 23){
+    let threeDescription = convertedRank.simpleDescription()
+}
+else {
+    print("no value found")
+}
+
+enum Suit {
+    case spades, hearts, diamonds, clubs
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .spades:
+            return "spades"
+        case .hearts:
+            return "hearts"
+        case .diamonds:
+            return "diamonds"
+        case .clubs:
+            return "clubs"
+        }
+    }
+    func color() -> String {
+        switch self{
+        case .hearts, .diamonds:
+            return "red"
+        case .clubs, .spades:
+            return "black"
+        }
+    }
+}
+
+let hearts = Suit.hearts
+let heartsDescription = hearts.simpleDescription()
+let heartsColor = hearts.color()
+
+let clubs = Suit.clubs
+let clubsDescription = clubs.simpleDescription()
+let clubsColor = clubs.color()
