7680bb351b7e804839a385aa6b10218bf3230154
Yulia Shidlovskaya <yulia@>
2019-06-07 20:06:31 -0700

    add Array playground

---
+++ Arrays.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var someInts = [Int]()
+someInts.append(3)
+someInts.count
+someInts = [] // empty, but still can contain only Ints
+
+var threeDoubles = Array(repeating: 0.0, count: 3)
+var anotherThreeDoubles = Array(repeating: 3.0, count: 3)
+var sixDoubles = threeDoubles + anotherThreeDoubles
+
+
